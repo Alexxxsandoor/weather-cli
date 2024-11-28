84b07011a3bbf280720664a3072681a181e335ee
ALEXXXSANDOOR <v.verstukov14@gmail.com>
2024-11-28 18:36:47 +0200

    work with 'path'

---
+++ services/storage.service.js
@@ -0,0 +1,10 @@
+import { homedir } from 'os';
+import { join } from 'path';
+
+const FILE_PATH = join(homedir(), 'weather-data.json')
+
+const saveKeyValue = (key, value) => {
+
+};
+
+export {saveKeyValue}
